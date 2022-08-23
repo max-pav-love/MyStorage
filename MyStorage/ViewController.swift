1be2520bef5821676d32cab34cda14b7b098a5c6
Max Pavlovich <max.pav.love@icloud.com>
2022-08-23 23:58:27 +0300

    Initial Commit

---
+++ MyStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStorage
+//
+//  Created by Максим Хлесткин on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
